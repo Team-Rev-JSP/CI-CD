4fcadde9ba747dab20ca9673dcee2fe15fb7583a
SM\sm <adszzz11@naver.com>
2021-05-21 01:21:07 +0900

    main / CICD shell 추가

---
+++ shel/backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'hi'
+sudo rm -rf /usr/local/tomcat/webapps/ROOT/backend
+
+sudo cp -ra /var/lib/jenkins/workspace/backend /usr/local/tomcat/webapps/ROOT/
+
+sudo sh /usr/local/tomcat/bin/shutdown.sh
+
+sudo sh /usr/local/tomcat/bin/startup.sh
