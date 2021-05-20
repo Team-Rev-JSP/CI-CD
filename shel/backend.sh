49e6d528b175ed2d01c8a61a4197795ed3895396
SM\sm <adszzz11@naver.com>
2021-05-21 01:11:39 +0900

    main / CICD Test 24

--- shel/backend.sh
+++ shel/backend.sh
@@ -2,7 +2,7 @@
 echo 'hi'
 sudo rm -rf /usr/local/tomcat/webapps/ROOT/backend
 
-sudo cp /var/lib/jenkins/workspace/backend /usr/local/tomcat/webapps/ROOT/
+sudo cp -ra /var/lib/jenkins/workspace/backend /usr/local/tomcat/webapps/ROOT/
 
 sudo sh /usr/local/tomcat/bin/shutdown.sh
 
