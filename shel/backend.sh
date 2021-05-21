9d35dcf1e539a789c97fdfc444ac44d1ff14ef0a
SM\sm <adszzz11@naver.com>
2021-05-21 23:11:43 +0900

    backend / 21-05-21 / backend init sh troubleshoot

--- shel/backend.sh
+++ shel/backend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'hi'
-sudo rm -rf /usr/local/tomcat/webapps/ROOT/backend/backend
+sudo rm -rf /usr/local/tomcat/webapps/ROOT/backend
 
-sudo cp -ra /var/lib/jenkins/workspace/backend /usr/local/tomcat/webapps/ROOT/
+sudo cp -ra /var/lib/jenkins/workspace/backend/backend /usr/local/tomcat/webapps/ROOT/
 
 sudo sh /usr/local/tomcat/bin/shutdown.sh
 
