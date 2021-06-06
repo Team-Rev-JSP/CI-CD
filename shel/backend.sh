cd67b62ce503d7adf6611b8819146c8bac8e26ea
SM\sm <adszzz11@naver.com>
2021-06-06 21:33:18 +0900

    main / CICD 최종

--- shel/backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'hi'
-sudo rm -rf /usr/local/tomcat/webapps/ROOT/backend
-
-sudo cp -ra /var/lib/jenkins/workspace/backend/backend /usr/local/tomcat/webapps/ROOT/
-
-sudo sh /usr/local/tomcat/bin/shutdown.sh
-
-sudo sh /usr/local/tomcat/bin/startup.sh
--- shel/frontend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo rm -rf /usr/local/tomcat/webapps/ROOT/frontend
-
-sudo cp -ra /var/lib/jenkins/workspace/frontend/frontend /usr/local/tomcat/webapps/ROOT/
-
-sudo sh /usr/local/tomcat/bin/shutdown.sh
-
-sudo sh /usr/local/tomcat/bin/startup.sh
