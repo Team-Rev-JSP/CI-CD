3893877f027f34352d2825f24ada94d37145263a
Sangmin <adszzz11@naver.com>
2021-06-09 11:22:35 +0900

    main / CICD 검색 2

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo rm -rf /usr/local/tomcat/webapps/ROOT
 
+sudo rm -rf /usr/local/tomcat/webapps/ROOT.war
+
 sudo cp -ra /var/lib/jenkins/workspace/backend/ROOT.war /usr/local/tomcat/webapps/
 
 sudo sh /usr/local/tomcat/bin/shutdown.sh
